038953e7570616b84978e2f51f200d44e912e92d
nazifanishat09 <nazifanishat2024@gmail.com>
2025-12-09 00:03:48 +0600

    2

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_docket_app/home_screen/Home_Screen.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -7,6 +8,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  NextScreen()async{
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder:(a)=> HomeScreen()));
+  }
+  @override
+  void initState() {
+   NextScreen();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
