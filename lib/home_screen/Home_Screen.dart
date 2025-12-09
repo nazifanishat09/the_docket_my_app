fed22bc57fae9ed33866bd8593be2c676ed6e7e7
nazifanishat09 <nazifanishat2024@gmail.com>
2025-12-10 00:52:48 +0600

    3

--- lib/home_screen/Home_Screen.dart
+++ lib/home_screen/Home_Screen.dart
@@ -11,7 +11,24 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(),
+      body: ListView(
+        children: [
+          InkWell(
+            onLongPress: () {
+              showDialog(
+                context: context,
+                builder: (a) => AlertDialog(
+                  title: Text("Confirm action",),
+                  content: Text("Are you sure that you want to permanently delete this note?"),
+                  actions: [Text("Delete"),
+                    Text("Cancel")],
+                ),
+              );
+            },
+            child: Card(child: Text("data")),
+          ),
+        ],
+      ),
     );
   }
 }
