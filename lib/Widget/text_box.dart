d769097c55e61c234eaaa116a2fc7e1c683e60bc
nazifanishat09 <nazifanishat2024@gmail.com>
2025-12-10 14:29:28 +0600

    5

---
+++ lib/Widget/text_box.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextBox extends StatelessWidget {
+  const TextBox({super.key, required this.title, this.maxline, this.fs, this.fw, this.color});
+  final String title;
+  final int? maxline;
+  final double? fs;
+  final FontWeight? fw;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title ,
+      maxLines: maxline,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontWeight: fw,
+        fontSize: fs,
+        color: color,
+      ),
+    );
+  }
+}
--- lib/home_screen/Home_Screen.dart
+++ lib/home_screen/Home_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -18,7 +19,10 @@ class _HomeScreenState extends State<HomeScreen> {
               showDialog(
                 context: context,
                 builder: (a) => AlertDialog(
-                  title: Text("Confirm action",),
+                  title:    TextBox(
+                    title: 'Confirm Action',
+                    fw: FontWeight.w900,
+                  ),
                   content: Text("Are you sure that you want to permanently delete this note?"),
                   actions: [Text("Delete"),
                     Text("Cancel")],
@@ -32,3 +36,29 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+class TextBox extends StatelessWidget {
+  const TextBox({super.key, required this.title, this.maxline, this.fs, this.fw, this.color});
+  final String title;
+  final int? maxline;
+  final double? fs;
+  final FontWeight? fw;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title ,
+      maxLines: maxline,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontWeight: fw,
+        fontSize: fs,
+        color: color,
+      ),
+    );
+  }
+}
+
